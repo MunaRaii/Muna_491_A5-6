4d094d5b726486a9f5bd1a020c1075de93e4c856
munarai <munaraii1996@gmail.com>
2023-11-24 16:37:08 -0500

    method for calculating turn in the race

--- car.js
+++ car.js
@@ -9,14 +9,27 @@ class Car{
         this.gas = gas;
     }
 
+    // display honk sound and information about car
     honk(){
         console.log('Tuut tuut');
         console.log(`The brand ${this.brand} ,${this.model} model came with astonishing ${this.color} color is priced at $${this.price} in ${this.year} year`);
     }
 
+    // calculate the turns in the race
+    race(){
+
+        // calculate amount of gas to be deducted on the basis of car's age
+        let gasLoss = 5;
+        if(this.year < new Date().getFullYear()){
+            gasLoss += (new Date().getFullYear() - this.year);
+        }
+        this.gas -= gasLoss;
+        console.log(`${this.brand} of ${this.model} has {this.gas} liters of gas remaining`);
+    }
+
 }
 
-// invoking class through this objects
+// car objects
 const honda = new Car('Honda', 'CR-V', 2023, 'Red', 50000, 45);
 const ford = new Car('Ford', 'F-150', 2020, 'Black', 25000, 30);
 const bmw = new Car('BMW', 'X5', 2022, 'Green', 60000, 65);
@@ -32,3 +45,13 @@ mazda.honk();
 audi.honk();
 kia.honk();
 
+// race for 7 turns
+for(let turn = 1; turn <= 7; turn++){
+    console.log(`\nTurn ${turn}: `);
+    honda.race();
+    ford.race();
+    bmw.race();
+    mazda.race();
+    audi.race();
+    kia.race();
+}
