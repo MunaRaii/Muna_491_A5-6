25a1fd5978b1d6e93a29bf293922f84076efabf0
munarai <munaraii1996@gmail.com>
2023-11-24 16:30:57 -0500

    created objects

--- car.js
+++ car.js
@@ -17,5 +17,18 @@ class Car{
 }
 
 // invoking class through this objects
-const honda = new Car('Honda', 'MN-E', 2020, 'White', 40000, 50);
+const honda = new Car('Honda', 'CR-V', 2023, 'Red', 50000, 45);
+const ford = new Car('Ford', 'F-150', 2020, 'Black', 25000, 30);
+const bmw = new Car('BMW', 'X5', 2022, 'Green', 60000, 65);
+const mazda = new Car('Mazda', 'CX-5', 2019, 'White', 15000, 60);
+const audi = new Car('Audi', 'Q7', 2018, 'Silver', 52000, 47);
+const kia = new Car('Kia', 'Forte', 2020, 'blue', 21000, 56);
+
+// invoking honk() for each car
 honda.honk();
+ford.honk();
+bmw.honk();
+mazda.honk();
+audi.honk();
+kia.honk();
+
